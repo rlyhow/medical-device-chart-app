3d5cfa4688836a8acaa8a82715a75a49a9f37c63
Mikita <gentled.druzhinnik@gmail.com>
2022-05-15 13:46:19 +0300

    feat: implement json parser

---
+++ MedicalDeviceChartApp/MedicalDeviceChartApp/Services/JsonParser.swift
@@ -0,0 +1,26 @@
+//
+//  JsonParser.swift
+//  MedicalDeviceChartApp
+//
+//  Created by Mikita Shalima on 15.05.22.
+//
+
+import Foundation
+
+class JsonParser {
+    
+    func loadJson(filename fileName: String) -> MedicineDevice? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(MedicineDevice.self, from: data)
+                return jsonData
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+    
+}
