f158000461d4d42075808b3357967fde1ffca1f0
Mikita <gentled.druzhinnik@gmail.com>
2022-05-16 16:19:45 +0300

    refactor: remove some comments

--- MedicalDeviceChartApp/MedicalDeviceChartApp/MedicalDeviceChartAppApp.swift
+++ MedicalDeviceChartApp/MedicalDeviceChartApp/MedicalDeviceChartAppApp.swift
@@ -20,7 +20,7 @@ struct MedicalDeviceChartAppApp: App {
 
 class AppDelegate: NSObject, UIApplicationDelegate {
         
-    static var orientationLock = UIInterfaceOrientationMask.all //By default you want all your views to rotate freely
+    static var orientationLock = UIInterfaceOrientationMask.all
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return AppDelegate.orientationLock
